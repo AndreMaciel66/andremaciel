232685d83a9593bb9a3700a744ee96060d9d9c01
AndreMaciel66 <andretmaciel@gmail.com>
2023-07-30 17:16:37 -0300

    adding new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), mdx(), svelte()]
+});
